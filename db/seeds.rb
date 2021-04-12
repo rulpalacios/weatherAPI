c71e4934e8d58f55eedf8c2a04e0792a0ff8a906
Raul Palacios <palacios.raul02@gmail.com>
2021-04-12 00:11:04 -0500

    Add seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+location = Location.create(name: 'New York City')
+
+location.recordings.create(temp: 32, status: 'cloudy')
+location.recordings.create(temp: 34, status: 'cloudy')
+location.recordings.create(temp: 30, status: 'rainy')
+location.recordings.create(temp: 28, status: 'cloudy')
+location.recordings.create(temp: 22, status: 'sunny')
